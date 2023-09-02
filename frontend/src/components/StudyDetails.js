5fca4f56902403476b709df4096090a5cb11886f
Daryl Agustin <darylagustin@DARYLs-MacBook-Air.local>
2023-09-02 22:44:32 +0800

    auth

--- frontend/src/components/StudyDetails.js
+++ frontend/src/components/StudyDetails.js
@@ -1,31 +1,44 @@
 import { useStudyContext } from "../hooks/useStudyContext";
 // date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const StudyDetails = ({ task }) => {
-    const maxCharacters = 30
-    const { dispatch } = useStudyContext( )
+  const maxCharacters = 30;
+  const { dispatch } = useStudyContext();
 
-    const handleClick = async () => {
-        const response = await fetch('/api/tasks/' + task._id, {
-            method: 'DELETE'
-        })
-        const json = await response.json()
+  const handleClick = async () => {
+    const response = await fetch("/api/tasks/" + task._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
 
-        if (response.ok) {
-            dispatch({type: 'DELETE_TASK', payload: json})
-        }
+    if (response.ok) {
+      dispatch({ type: "DELETE_TASK", payload: json });
     }
-    
-    return (
-        <div className="study-details">
-            <h4>{task.topic}</h4>
-            <p><strong>Description:</strong>&nbsp;{task.description}</p>
-            <p><strong>Link:</strong>&nbsp;<a href={task.link}>{task.link.length > maxCharacters ? task.link.substring(0, maxCharacters) + '\u2026' : task.link}</a></p>
-            <p>{formatDistanceToNow(new Date(task.createdAt), { addSuffix: true })}</p>
-            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
-        </div>
-    )
-}
+  };
 
-export default StudyDetails
+  return (
+    <div className="study-details">
+      <h4>{task.topic}</h4>
+      <p>
+        <strong>Description:</strong>&nbsp;{task.description}
+      </p>
+      <p>
+        <strong>Link:</strong>&nbsp;
+        <a href={task.link}>
+          {task.link.length > maxCharacters
+            ? task.link.substring(0, maxCharacters) + "\u2026"
+            : task.link}
+        </a>
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(task.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
+    </div>
+  );
+};
+
+export default StudyDetails;
