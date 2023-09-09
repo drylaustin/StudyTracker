f14575d4e524eb0bc0ce448f27c524f41407ea73
Daryl Agustin <darylagustin@DARYLs-MacBook-Air.local>
2023-09-09 21:29:27 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const taskRoutes = require("./routes/tasks");
 const userRoutes = require("./routes/user");
+const cors = require('cors')
+
 
 // express app
 const app = express();
 
-const { API_URL } = process.env
+app.use(
+  cors({
+    origin: "https://study-tracker-frontend.vercel.app"
+}))
   
 // middleware
 app.use(express.json());
@@ -35,29 +40,29 @@ mongoose
     console.log(error);
   });
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+// const allowCors = fn => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
+// const handler = (req, res) => {
+//   const d = new Date()
+//   res.end(d.toString())
+// }
 
-module.exports = allowCors(handler)
+// module.exports = allowCors(handler)
 
 
 module.exports = app;
